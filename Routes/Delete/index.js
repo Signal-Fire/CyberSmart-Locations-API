1a8db9d4abff8a9dd52b19ffc79d2cc730654501
Henry <henrypye@outlook.com>
2018-03-18 22:32:05 +0000

    Added DeleteHandler and Delete Routes

---
+++ Routes/Delete/index.js
@@ -0,0 +1,29 @@
+/* jshint esversion: 6*/
+var route = require('express').Router();
+var Deleter = new(require('../../Handlers/Delete'))();
+
+route.post('/:id', function(req, res) {
+    try {
+        Deleter.DeleteById(req.params.id).then(deleted => {
+            res.status(200).send("{ success : " + deleted + " }");
+        }).catch(error => {
+            res.status(401).send("{ error : " + error + " }");
+        });
+    } catch (ex) {
+        res.status(500).send("{ error : " + ex + " }");
+    }
+});
+
+route.post('/:name', function(req, res) {
+    try {
+        Deleter.DeleteByName(req.params.name).then(deleted => {
+            res.status(200).send("{ success : " + deleted + " }");
+        }).catch(error => {
+            res.status(401).send("{ error : " + error + " }");
+        });
+    } catch (ex) {
+        res.status(500).send("{ error : " + ex + " }");
+    }
+});
+
+module.exports = route;
